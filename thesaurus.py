0a7274b3bb8f2807e541ea5806d0648787a89f3f
dataminer-x <2nakedj@gmail.com>
2020-04-18 16:44:39 -0400

    testing remove parameter check

--- thesaurus.py
+++ thesaurus.py
@@ -12,18 +12,18 @@ class Thesaurus(NeuronModule):
         if not self._is_parameters_ok():
             raise InvalidParameterException
 
-    def _is_parameters_ok(self):
-        """
-        Check if received parameters are ok to perform operations in the neuron
-        :return: true if parameters are ok, raise an exception otherwise
-        .. raises:: NotImplementedError
-        """
-        if self.api_key is None:
-            raise MissingParameterException("Please provide an api_key")
-        if self.word is None:
-            raise MissingParameterException("Please provide a word")
-
-        return True
+    # def _is_parameters_ok(self):
+    #     """
+    #     Check if received parameters are ok to perform operations in the neuron
+    #     :return: true if parameters are ok, raise an exception otherwise
+    #     .. raises:: NotImplementedError
+    #     """
+    #     if self.api_key is None:
+    #         raise MissingParameterException("Please provide an api_key")
+    #     if self.word is None:
+    #         raise MissingParameterException("Please provide a word")
+    #
+    #     return True
 
     def _get_ref_data(self):
         url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + word + '?key=' + api_key
